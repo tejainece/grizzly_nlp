c3fdd41bbbdbfc6858c9d8a729a2f2ad64b7a130
Ravi Teja Gudapati <tejainece@gmail.com>
2018-04-12 20:43:22 +0200

    CountTokenizer

--- lib/src/vectorizer/vectorizer.dart
+++ lib/src/vectorizer/vectorizer.dart
@@ -1,10 +1,14 @@
 import 'package:grizzly_array/grizzly_array.dart';
+import 'package:grizzly_series/grizzly_series.dart';
 import '../tokenizer/simple_tokenizer.dart';
 
-Int1D countVectorize(List<String> documents,
-    {Tokenizer tokenizer: const WordPunctuationTokenizer()}) {
-  for(String doc in documents) {
-    new String1D(tokenizer.tokenize(doc));
+IntSeries<String> countVectorize(List<String> documents,
+    {Tokenizer tokenizer: const WordPunctuationTokenizer(),
+    Set<String> stopWords}) {
+  for (String doc in documents) {
+    final String1D tokens = new String1D(tokenizer.tokenize(doc))
+      ..removeMany(new IterView(stopWords));
+    tokens.valueCounts();
   }
   // TODO
 }
